19c692bf4373f7c4cb99000d2f1b9943c1d70b8a
gulsha-2613 <iamgulshan9799@gmail.com>
2025-10-18 17:54:21 +0530

    Completed city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,26 +1,59 @@
 const { City } = require('../models/index'); 
 
 class CityRepository {
+
     async createCity({ name }) {
         try {
-            const city = await City.create({name});
+            const city = await City.create({
+                name
+            });
             return city;
-        } catch (error){
-             throw {error};
-        }
+        } catch (error) {
+            console.log("Something went wrong in the repository layer");
+            throw {error};
+        } 
     }
 
-    async deleteCity({ cityId }) {
+    async deleteCity(cityId) {
         try {
             await City.destroy({
                 where: {
                     id: cityId
                 }
             });
-        } catch (error){
+            return true;
+        } catch (error) {
+            console.log("Something went wrong in the repository layer");
             throw {error};
         }
     }
+
+    async updateCity(cityId, data) {
+        try {
+            const city = await City.update(data, {
+                where: {
+                    id: cityId
+                }
+            });
+            return city;
+        } catch (error) {
+            console.log("Something went wrong in the repository layer");
+            throw {error};
+        }
+    }
+    
+    async getCity(cityId) {
+        try {
+            const city = await City.findByPk(cityId);
+            return City;
+        } catch (error) {
+            console.log("Something went wrong in the repository layer");
+            throw {error};
+            
+        }
+    }
+
+
 }
 
 module.exports = CityRepository;
